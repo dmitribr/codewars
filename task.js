a6ca8546b6f8572f167c69586e87dc25654e7cb4
Dmitrybr <dmiitriib@gmail.ru>
2023-11-19 20:46:32 +0900

    task4CW

--- task.js
+++ task.js
@@ -12,3 +12,30 @@ function squareOrSquareRoot(array) {
   }
   var input = [ 4, 3, 9, 7, 2, 1 ]
   squareOrSquareRoot(input);
+
+  //вернуть приветсвие.
+  function greet(name){
+    return `Hello, ${name} how are you doing today?`;
+  }
+  greet();
+
+  //вернуть true если первые и последняие буквы совпадают.
+  function feast2(beast, dish) {
+    let gf = beast.toLowerCase().split('');
+    let gf2 = dish.toLowerCase().split('');
+        if (gf[gf.length -1] === gf2[gf2.length -1] && gf[0] === gf2[0]) {
+                return true;
+            }
+            return false;
+    }
+feast2('hggfgff555', 'hgfhg5');
+
+//рассчитать силы по видам и вернуть за кем будет подеба.
+function goodVsEvil(good, evil){
+    var goodArr = good.split(' '),
+     evilArr = evil.split(' '),
+     goodPoints = goodArr[0]*1 + goodArr[1]*2 + goodArr[2]*3 + goodArr[3]*3 + goodArr[4]*4 + goodArr[5]*10,
+     evilPoints = evilArr[0]*1 + evilArr[1]*2 + evilArr[2]*2 + evilArr[3]*2 + evilArr[4]*3 + evilArr[5]*5 + evilArr[6]*10;
+   if (goodPoints === evilPoints) return 'Battle Result: No victor on this battle field';
+    return goodPoints < evilPoints ?  "Battle Result: Evil eradicates all trace of Good" :"Battle Result: Good triumphs over Evil";
+}
